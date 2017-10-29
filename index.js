c7036ec239055cbf61003335ea4d7a3a2d0ef813
jstass <jasonstass@gmail.com>
2017-10-28 21:56:52 -0500

    Initial upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+const log = console.log;
+const style1 = chalk.white.bgBlue.bold
+log(style1("Aoccdrnig to a rscheearch at Cmabrigde Uinervtisy") + "\nit deosn't mttaer in waht oredr the ltteers in a wrod are in" + chalk.red("."));
+log(chalk.whiteBright.bgGreenBright.bold("The olny iprmoatnt tihng is taht the frist and lsat ltteers be at the rghit pclae."));
+log(chalk.strikethrough.redBright.bgWhiteBright.bold("The rset can be a toatl mses and you can sitll raed it wouthit porbelm."));
+log(chalk.underline.cyan.bold("Tihs is bcuseae the huamn mnid deos not raed ervey lteter by istlef, but the wrod as a wlohe."));
